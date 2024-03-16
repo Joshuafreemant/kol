7aae8e0731f422541ca66b3b5f357c5da88a69bc
Akinniyi Tolulope <47461817+Joshuafreemant@users.noreply.github.com>
2024-03-16 11:58:01 +0100

    debugging 2

--- app/lib/fetch.ts
+++ app/lib/fetch.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 export const apiInstance = axios.create({
 	// Configuration
-	baseURL: "http://localhost:3000/api",
+	// baseURL: "http://localhost:3000/api",
+	baseURL: "https://kol-fvx7g3n9a-joshuafreemant.vercel.app/api",
+
     withCredentials: true,
 	headers: {
 		Accept: 'application/json',
